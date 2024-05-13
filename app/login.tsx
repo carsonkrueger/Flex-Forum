ffebc3b653e7976e38898ec954e7710f172e5b67
carsonkrueger <crkruege@gmail.com>
2024-05-12 21:37:17 -0600

    init commit

---
+++ app/login.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Page() {
+  return (
+    <View style={{ height: "auto", backgroundColor: "blue" }}>
+      <Text>Login</Text>
+    </View>
+  );
+}
